29e34147c14e51845da0d70608cb492eb45979e4
Roman Bach <roman.bach@amcon.de>
2025-05-22 14:32:52 +0200

    shop

---
+++ Shop.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+class ShopFenster:
+    def __init__(self, master):
+        self.master = master
+        self.fenster = tk.Toplevel(master)
+        self.fenster.title("Shop")
+        self.fenster.geometry("400x300")
+        self.fenster.state('zoomed')
+
+        tk.Label(self.fenster, text="🛒 Willkomen im Shop!",bg="blue", font=("Arial", 18)).pack(pady=20)
+
+        lbl_Item1 = tk.Label(self.fenster,bg="blue", font=("Press Start 2P", 1), width= 120, height= 120)
+        lbl_Item1.place(x=250, y=100)  # Position in Pixeln (x= Breite Y= Höhe)
+
+        lbl_Item2 = tk.Label(self.fenster,bg="blue", font=("Press Start 2P", 1), width= 120, height= 120)
+        lbl_Item2.place(x=500, y=100)  # Position in Pixeln (x= Breite Y= Höhe)
+
+        lbl_Item3 = tk.Label(self.fenster,bg="blue", font=("Press Start 2P", 1), width= 260, height= 120)
+        lbl_Item3.place(x=750, y=100)  # Position in Pixeln (x= Breite Y= Höhe)
+
+        lbl_Item4 = tk.Label(self.fenster,bg="blue", font=("Press Start 2P", 1), width= 120, height= 60)
+        lbl_Item4.place(x=250, y=500)  # Position in Pixeln (x= Breite Y= Höhe)
+
+        lbl_Item5 = tk.Label(self.fenster,bg="blue", font=("Press Start 2P", 1), width= 120, height= 60)
+        lbl_Item5.place(x=500, y=500)  # Position in Pixeln (x= Breite Y= Höhe)
+
+        lbl_Item6 = tk.Label(self.fenster,bg="blue", font=("Press Start 2P", 1), width= 260, height= 60)
+        lbl_Item6.place(x=750, y=500)  # Position in Pixeln (x= Breite Y= Höhe)
+
+        # Button zum Zurückkehren
+        tk.Button(self.fenster, text="Zurück zum Menü", command=self.zurück).pack(pady=20)
+
+    def zurück(self):
+        self.fenster.destroy()  # Shop-Fenster schließen
+        self.master.deiconify()  # Hauptfenster wieder sichtbar machen
+        self.master.state('zoomed')
+
+
+    def kaufe_punkte(self):
+        print("Punkte gekauft!")  # später: Punktestand aktualisieren
--- StartWindow.py
+++ StartWindow.py
@@ -1,25 +1,15 @@
 from SlotMachine import SlotMachineFenster
+from Shop import ShopFenster
+
 from tkinter import Button
 
 from PIL import Image, ImageTk
 import tkinter as tk
 
 def SECRETBUTTON():
-    label.config(text="GRUPPE ALKOHOLIKER \nSTINKT AB")
+    lbl_Titel.config(text="GRUPPE ALKOHOLIKER  \nSTINKT AB  ")
     # Nach 1000 ms (1 Sekunde) Text zurücksetzen
-    root.after(1000, lambda: label.config(text=" Bürgermeisters \nSpielplatz"))
-
-def neues_fenster_öffnen():
-    neues_fenster = tk.Toplevel()         # Neues Fenster
-    neues_fenster.title("Zweites Fenster")
-    neues_fenster.geometry("300x200")
-
-    label = tk.Label(neues_fenster, text="Willkommen im neuen Fenster!")
-    label.pack(pady=20)
-
-    schliessen_button = tk.Button(neues_fenster, text="Schließen", command=neues_fenster.destroy)
-    schliessen_button.pack()
-
+    root.after(1000, lambda: lbl_Titel.config(text=" Bürgermeisters \nSpielplatz"))
 
 root = tk.Tk()
 root.title("Bürgermeisters Spielplatz") # Fenster Name
@@ -35,21 +25,20 @@ bild_tk = ImageTk.PhotoImage(bild)    # Konvertieren für Tkinter
 ButtonSECRET = tk.Button(root, font=("Press Start 2P",1), relief="flat", width= 50, height= 22,command=SECRETBUTTON)
 ButtonSECRET.place(x=310, y=12)
 
-label2 = tk.Label(root, image=bild_tk)
-label2.image = bild_tk  # WICHTIG: Referenz speichern!
-label2.place(x=298, y=1)
+lbl_bild = tk.Label(root, image=bild_tk)
+lbl_bild.image = bild_tk  # WICHTIG: Referenz speichern!
+lbl_bild.place(x=298, y=1)
 
 #Label mit text
-label = tk.Label(root, text=" Bürgermeisters \nSpielplatz", font=("Press Start 2P", 50))
-label.place(x=250, y=80)  # Position in Pixeln (x= Breite Y= Höhe)
+lbl_Titel = tk.Label(root, text=" Bürgermeisters \nSpielplatz", font=("Press Start 2P", 50))
+lbl_Titel.place(x=250, y=80)  # Position in Pixeln (x= Breite Y= Höhe)
 
 #Button
 
+Btn_Slot = tk.Button(root, text= "SLOTTI KAROTTI", font=("Press Start 2P",30),command=lambda: [SlotMachineFenster(root), root.withdraw()], relief="solid",bd= 3, width= 30, height= 1, bg="lightgrey")
+Btn_Slot.place(x=170, y=280)
 
-Button1 = tk.Button(root, text= "SLOTTI KAROTTI", font=("Press Start 2P",30),command=lambda: SlotMachineFenster(root), relief="solid",bd= 3, width= 30, height= 1, bg="lightgrey")
-Button1.place(x=170, y=280)
-
-Button2 = tk.Button(root, text= "CAPTAIN BLACK", font=("Press Start 2P",30),relief="solid", width= 30,bd= 3, height= 1, bg="lightgrey")
+Button2 = tk.Button(root, text= "[coming soon...]", font=("Press Start 2P",30),relief="solid", width= 30,bd= 3, height= 1, bg="lightgrey")
 Button2.place(x=170, y=380)
 
 Button3 = tk.Button(root, text= "[coming soon...]", font=("Press Start 2P",30),relief="solid", width= 30,bd= 3, height= 1, bg="lightgrey")
@@ -58,7 +47,7 @@ Button3.place(x=170, y=480)
 Button4 = tk.Button(root, text= "KIPPENAUTOMAT", font=("Press Start 2P",30),relief="solid", width= 30,bd= 3, height= 1, bg="lightgrey")
 Button4.place(x=170, y=580)
 
-Button5 = tk.Button(root, text= "STORE", font=("Press Start 2P",30), width= 30,relief="solid", height= 1,bd= 3, bg="lightgrey")
-Button5.place(x=170, y=680)
+Btn_Shop = tk.Button(root, text= "STORE", font=("Press Start 2P",30), command=lambda: [ShopFenster(root), root.withdraw()], width= 30,relief="solid", height= 1,bd= 3, bg="lightgrey")
+Btn_Shop.place(x=170, y=680)
 
 root.mainloop()
